a7fdf41885186b93547f14c8cb6aca0cf12f367f
sztoslol <kubuszeski05@wp.pl>
2023-05-15 15:44:23 +0200

    7 commit

--- app/src/main/java/com/example/projekt_zdjecia/MainActivity.kt
+++ app/src/main/java/com/example/projekt_zdjecia/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,32 +14,48 @@ class MainActivity : AppCompatActivity() {
         val wstecz = findViewById<Button>(R.id.wstecz)
         val dalej = findViewById<Button>(R.id.dalej)
         val zdj = findViewById<ImageView>(R.id.imageView)
+        val tekst = findViewById<TextView>(R.id.textView)
+
         var i=1;
         dalej.setOnClickListener {
             i++;
-            if (i==1)
+            if (i==1) {
                 zdj.setImageResource(R.drawable.kwiatya)
-            else if (i==2)
+                tekst.text = "Obrazek 1/3"
+            }
+            else if (i==2){
                 zdj.setImageResource(R.drawable.kwiatyb)
-            else if (i==3)
+                tekst.text = "Obrazek 2/3"
+            }
+            else if (i==3){
                 zdj.setImageResource(R.drawable.kwiatyc)
+                tekst.text = "Obrazek 3/3"
+            }
             else if (i==4) {
                 i = 1
                 zdj.setImageResource(R.drawable.kwiatya)
+                tekst.text = "Obrazek 1/3"
             }
         }
 
         wstecz.setOnClickListener {
             i--
-            if (i==1)
+            if (i==1){
                 zdj.setImageResource(R.drawable.kwiatya)
-            else if (i==2)
+                tekst.text = "Obrazek 1/3"
+            }
+            else if (i==2){
                 zdj.setImageResource(R.drawable.kwiatyb)
-            else if (i==3)
+                tekst.text = "Obrazek 2/3"
+            }
+            else if (i==3){
                 zdj.setImageResource(R.drawable.kwiatyc)
+                tekst.text = "Obrazek 3/3"
+            }
             else if (i==0) {
                 i = 3
                 zdj.setImageResource(R.drawable.kwiatyc)
+                tekst.text = "Obrazek 3/3"
             }
         }
 
