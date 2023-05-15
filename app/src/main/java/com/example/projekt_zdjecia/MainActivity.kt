6d336ff2e3f76d98013fc5f29db17dd96f3ee427
sztoslol <kubuszeski05@wp.pl>
2023-05-15 15:10:26 +0200

    3 commit

--- app/src/main/java/com/example/projekt_zdjecia/MainActivity.kt
+++ app/src/main/java/com/example/projekt_zdjecia/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.projekt_zdjecia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val wstecz = findViewById<Button>(R.id.wstecz)
+        val dalej = findViewById<Button>(R.id.dalej)
+        var i=1;
+        dalej.setOnClickListener {
+            i++;
+        }
+        wstecz.setOnClickListener {
+            i--
+        }
 
     }
 }
