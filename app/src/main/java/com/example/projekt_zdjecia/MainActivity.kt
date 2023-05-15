a3f2cef8d177acf92cb7f934c2825dcbeb2f194b
sztoslol <kubuszeski05@wp.pl>
2023-05-15 15:18:03 +0200

    4 commit

--- app/src/main/java/com/example/projekt_zdjecia/MainActivity.kt
+++ app/src/main/java/com/example/projekt_zdjecia/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.projekt_zdjecia
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
         val wstecz = findViewById<Button>(R.id.wstecz)
         val dalej = findViewById<Button>(R.id.dalej)
+        val zdj = findViewById<ImageView>(R.id.imageView)
         var i=1;
         dalej.setOnClickListener {
             i++;
@@ -18,6 +20,15 @@ class MainActivity : AppCompatActivity() {
         wstecz.setOnClickListener {
             i--
         }
-
+        if (i==1)
+            zdj.setImageResource(R.drawable.kwiatya)
+            else if (i==2)
+                zdj.setImageResource(R.drawable.kwiatyb)
+                else if (i==3)
+                    zdj.setImageResource(R.drawable.kwiatyc)
+                    else if (i==4)
+                        i=1
+                        else
+                            i=3
     }
 }
